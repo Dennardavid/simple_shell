f99ca00b908cecb9c9134e904ce954a9fa37ffdc
Wilfred Ashiagbor <wilfredonii45@gmail.com>
2023-04-26 12:01:00 +0000

    lists

--- lists.c
+++ lists.c
@@ -13,22 +13,86 @@ sep_node *add_separator_to_end(sep_node **list_head, char sep_char)
 	sep_node *new_node, *temp;
 
 	new_node = malloc(sizeof(sep_node));
-	if (new_node == NULL)
+	if (!new_node)
+	{
 		return (NULL);
+	}
 
 	new_node->separator = sep_char;
 	new_node->next = NULL;
 	temp = *list_head;
 
-	if (temp == NULL)
+	if (!temp)
 	{
 		*list_head = new_node;
 	}
 	else
 	{
 		while (temp->next != NULL)
+		{
 			temp = temp->next;
+		}
 		temp->next = new_node;
+	}
+
+	return (*list_head);
+}
+
+/**
+ * freeSeparator - frees a linked list of separator nodes
+ * @list_head: double pointer to the head of the list
+ */
+
+void freeSeparator(sep_node **list_head)
+{
+	sep_node *temp;
+	sep_node *current;
+
+	if (!list_head)
+	{
+		current = *list_head;
+		while ((temp = current))
+		{
+			current = current->next;
+			free(temp);
+		}
+		*list_head = NULL;
+	}
+}
+
+/**
+ * add_line_to_end - adds a line node to the end of a linked list
+ * @list_head: double pointer to the head of the list
+ * @line_str: line string to be added
+ *
+ * Return: pointer to the head of the list
+ */
+
+line_node *add_line_to_end(line_node **list_head, char *line_str)
+{
+	line_node *new_node, *temp_node;
+
+	new_node = malloc(sizeof(line_node));
+	if (!new_node)
+	{
+		return (NULL);
+	}
+
+	new_node->line = line_str;
+	new_node->next = NULL;
+	temp_node = *list_head;
+
+	if (!temp_node)
+	{
+		*list_head = new_node;
+	}
+	else
+	{
+		while (temp_node->next != NULL)
+		{
+			temp_node = temp_node->next;
+		}
+		temp_node->next = new_node;
 	}
 
 	return (*list_head);
