ae98664c4c931202b43df3f37f453576aea1cfc3
Wilfred Ashiagbor <wilfredonii45@gmail.com>
2023-04-26 16:21:38 +0000

    Lists 2

---
+++ more_lists.c
@@ -0,0 +1,84 @@
+#include "simple_shell.h"
+
+/**
+ * add_var_node - adds a variable node to a linked list
+ * @list_head: double pointer to the head of the list
+ * @var_len: length of the variable name
+ * @value: value of the variable
+ * @value_len: length of the value
+ *
+ * Return: pointer to the new node
+ */
+var_node *add_var_node(var_node **list_head, int var_len, char *value, int value_len)
+{
+	var_node *new_node;
+	var_node *temp_node;
+
+	new_node = malloc(sizeof(var_node));
+	if (!new_node)
+	{
+		return (NULL);
+	}
+
+	new_node->var_len = var_len;
+	new_node->value = value;
+	new_node->value_len = value_len;
+	new_node->next = NULL;
+
+	if (*list_head == NULL)
+	{
+		*list_head = new_node;
+	}
+	else
+	{
+		temp_node = *list_head;
+		while (temp_node->next != NULL)
+			temp_node = temp_node->next;
+		temp_node->next = new_node;
+	}
+
+	return (new_node);
+}
+
+/**
+ * free_var_node - frees a linked list of variable nodes
+ * @list_head: double pointer to the head of the list
+ */
+
+void free_var_node(var_node **list_head)
+{
+	var_node *temp_node;
+	var_node *curr_node;
+
+	if (!list_head)
+	{
+		current_node = *list_head;
+		while ((temp_node = current_node) != NULL)
+		{
+			current_node = current_node->next;
+			free(temp_node);
+		}
+		*list_head = NULL;
+	}
+}
+
+/**
+ * free_line_node - frees a linked list of line nodes
+ * @list_head: double pointer to the head of the list
+ */
+void free_line_node(line_node **list_head)
+{
+	line_node *temp_node;
+	line_node *current_node;
+
+	if (!list_head)
+	{
+		current_node = *list_head;
+		while ((temp_node = current_node) != NULL)
+		{
+			current_node = current_node->next;
+			free(temp_node);
+		}
+		*list_head = NULL;
+	}
+}
